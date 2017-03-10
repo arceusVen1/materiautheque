67a137195e517eaedb694ade17b0e6174f8922ce
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-03-10 22:13:01 +0100

    correct url reference parsing

--- materiaux/urls.py
+++ materiaux/urls.py
@@ -5,5 +5,5 @@ from materiaux import views
 
 urlpatterns = [
     url(r'^$', views.index, name='materiaux_paths'),
-    url(r'^(?P<reference>MAT-w{2}-[0-9]{2}-\d+)$', views.show_materiau, name='materiau_path'),
+    url(r'^(?P<reference>MAT-\w{2}-\d+-\d+)$', views.show_materiau, name='materiau_path'),
 ]
