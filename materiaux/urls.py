7fba37d258fb75678b91ca11385d0a2555e35adf
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-03-10 23:30:44 +0100

    url

--- materiaux/urls.py
+++ materiaux/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
 from materiaux import views
+from materiaux.models import Materiau
 
 urlpatterns = [
     url(r'^$', views.index, name='materiaux_paths'),
-    url(r'^(?P<reference>MAT-\w{2}-\d+-\d+)$', views.show_materiau, name='materiau_path'),
+    url(r'^nouveau/$', views.CreateMateriau.as_view(model=Materiau), name='new_materiau_path'),
+    url(r'^(?P<slug>MAT-\w{2}-\d+-\d+)/$', views.show_materiau, name='materiau_path'),
+    url(r'^(?P<slug>MAT-\w{2}-\d+-\d+)/edit/$', views.UpdateMateriau.as_view(model=Materiau), name='materiau_path'),
 ]
