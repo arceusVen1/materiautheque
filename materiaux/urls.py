c67f01143af41e24a92f1ba0e3c800b887b0da6d
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-03-10 21:32:41 +0100

    updated reference

--- materiaux/urls.py
+++ materiaux/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
+from materiaux import views
 
 urlpatterns = [
-    url(r'^$', views.homepage, name='homepage'),
+    url(r'^$', views.index, name='materiaux_paths'),
+    url(r'^(?P<reference>MAT-w{2}-[0-9]{2}-\d+)$', views.show_materiau, name='materiau_path'),
 ]
