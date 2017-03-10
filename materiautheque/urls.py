8daa32f9f28011169d50f2b54a3b90861a6b3263
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-03-10 21:32:51 +0100

    include

--- materiautheque/urls.py
+++ materiautheque/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
-from materiautheque import views
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^materiaux/', include (materiaux.urls)),
-    #url(r'^propriete/', include (propriete.urls)),
-    url(r'^$', views.homepage, name='homepage'),
+    url(r'^materiaux/', include ('materiaux.urls')),
+    #url(r'^propriete/', include ('propriete.urls')),
+    #url(r'^$', views.homepage, name='homepage'),
 ]
