25d11d3b87845da807f92b44b97d00ee6b86fca6
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-03-10 23:31:02 +0100

    create generic view

--- materiaux/views.py
+++ materiaux/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView
 from materiaux.models import Materiau, Famille, SousFamille
 
 
@@ -12,38 +12,38 @@ def index(request):
     return render(request, 'materiaux/index.html', {'materiaux': materiaux})
 
 
-def show_materiau(request, reference):
+def show_materiau(request, slug):
     """
     Retourne un materiau à la vue selon la référence
 
     :param request: the request
     :type request: HttpRequest
-    :param reference: la référence du matériau (MAT-FA-SS-ID)
-    :type reference: str
+    :param slug: la référence du matériau (MAT-FA-SS-ID)
+    :type slug: str
     :return: materiau/show.html
 
     :raises Http404: si le materiau n'existe pas
     """
     try:
-        mat = Materiau.objects.get(reference=reference)
+        mat = Materiau.objects.get(slug=slug)
     except Materiau.DoesNotExist:
         raise Http404("La référence de l'objet n'existe pas")
     return render(request, "materiaux/show.html", {'mat': mat})
 
 
-def delete_materiau(request, reference):
+def delete_materiau(request, slug):
     """
 
     :param request: la requête
     :type request: HttpRequest
-    :param reference: la référence du matériau (MAT-FA-SS-ID)
-    :type reference: str
+    :param slug: la référence du matériau (MAT-FA-SS-ID)
+    :type slug: str
     :return: redirige vers l'index
 
     :raises Http404: si le materiau n'existe pas
     """
     try:
-        mat = Materiau.objects.get(reference=reference)
+        mat = Materiau.objects.get(slug=slug)
     except Materiau.DoesNotExist:
         raise Http404("La référence de l'objet n'existe pas")
     mat.delete()
@@ -57,4 +57,12 @@ class UpdateMateriau(UpdateView):
     """
     model = Materiau
     fields = ['ss_famille', 'fournisseur', 'usage', 'normatif', 'disponible']
-    template_name_suffix = '_update_form'
+    template_name_suffix = '_form'
+
+class CreateMateriau(CreateView):
+    """
+    Permet la création d'un matériau de manière générique
+    """
+    model = Materiau
+    fields = ['ss_famille', 'fournisseur', 'usage', 'normatif', 'disponible']
+    template_name_suffix = '_form'
