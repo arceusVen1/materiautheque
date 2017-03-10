5510f2b7431205e8398f9d9a70a1154f37074c45
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-03-10 22:11:06 +0100

    admin model imports

--- materiaux/admin.py
+++ materiaux/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from materiaux.models import *
 # Register your models here.
+admin.site.register(Materiau)
+admin.site.register(Famille)
+admin.site.register(SousFamille)
