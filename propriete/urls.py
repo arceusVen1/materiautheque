a53a7d66e89bb19968d4a28564c3016a7af5c384
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-03-13 18:17:34 +0100

    debug

--- propriete/urls.py
+++ propriete/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
+from propriete import views
+from propriete.models import Propriete
 
 urlpatterns = [
-    url(r'^$', views.homepage, name='homepage'),
+    url(r'^$', views.index, name='proprietes_path'),
+    url(r'^nouveau/$', views.CreatePropriete.as_view(model=Propriete), name='new_propriete_path'),
+    url(r'^(?P<slug>\w+)/$', views.show_propriete, name='propriete_path'),
+    url(r'^(?P<slug>\w+)/edit/$', views.UpdatePropriete.as_view(model=Propriete), name='propriete_path'),
+    url(r'^(?P<slug>\w+)/delete/$', views.DeletePropriete.as_view(model=Propriete),
+        name='delete_propriete_path'),
+
 ]
