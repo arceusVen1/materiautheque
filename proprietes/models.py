0d80161cccdde07fb5746bf9f4f80e7db9633916
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-03-13 18:01:44 +0100

    unversioned + migrations + xml

---
+++ proprietes/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Propriete(models.Model):
+
+    class Meta:
+        verbose_name = "Propriété"
+        verbose_name_plural = "Propriétés"
+
+    slug = models.CharField(max_length=255, verbose_name="Nom", unique=True)
+    unite = models.CharField(max_length=30, verbose_name="Unité", default="N.R.")
+    definition = models.TextField(default="N.R.")
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        super().save()
+
+    def get_absolute_url(self):
+        return u'/proprietes/{}'.format(self.slug)
+
+    def __str__(self):
+        return self.slug
---
+++ proprietes/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.http import Http404
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from proprietes.models import Propriete
+
+# Create your views here.
+
+# Proprietes section --------------------------------------------------------------------------------------------------
+
+
+def index(request):
+    proprietes = Propriete.objects.all()
+    return render(request, "proprietes/index.html", {'proprietes': proprietes})
+
+
+def show_propriete(request, slug):
+    """
+    Retourne un materiau à la vue selon la référence
+
+    :param request: the request
+    :type request: HttpRequest
+    :param slug: le nom de la proprietes
+    :type slug: str
+    :return: proprietes/show.html
+
+    :raises Http404: si le materiau n'existe pas
+    """
+    try:
+        prop = Propriete.objects.get(slug=slug)
+    except Propriete.DoesNotExist:
+        raise Http404("La propriété n'existe pas")
+    return render(request, "proprietes/show.html", {'prop': prop})
+
+
+class UpdatePropriete(UpdateView):
+    """
+    Permet la mise à jour d'une proprietes via le template propriete_form.html
+    Inclu dans les urls par la méthode as_view()
+    """
+    model = Propriete
+    fields = ['slug', 'unite', 'definition']
+
+
+class CreatePropriete(CreateView):
+    """
+    Permet la création d'une proriete de manière générique
+    """
+    model = Propriete
+    fields = ['slug', 'unite', 'definition']
+
+
+class DeletePropriete(DeleteView):
+    """
+    Permet la suppression d'une propriété de manière générique
+    """
+    model = Propriete
+    success_url = reverse_lazy('proprietes_path')
