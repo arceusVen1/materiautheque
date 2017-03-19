912b6f2fc026c2f64f9a30251576e86940d232dd
arceusVen1 <jeremy.venin@telecom-sudparis.eu>
2017-03-19 12:47:09 +0100

    last materiau on homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from materiaux.models import Materiau
 
 # Create your views here.
 def home(request):
-    return render(request, 'materiautheque/homepage.html')
+    last_matx = Materiau.objects.order_by('date')[:5]
+    return render(request, 'materiautheque/homepage.html', {'last_matx': last_matx})
