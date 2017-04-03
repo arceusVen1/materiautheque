d8db443a8e505b9db89b92b20adab55b2121dc61
Jeremy VENIN <arceus@minet.net>
2017-04-03 18:07:38 +0200

    recherche multicritères

--- homepage/views.py
+++ homepage/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, render_to_response
 from materiaux.models import Materiau
 from homepage.form import SearchForm
+import re
 
 # Create your views here.
 def home(request):
     last_matx = Materiau.objects.order_by('date')[:5]
+    regexp = re.compile(r"\w+")
     search_form = SearchForm(request.POST or None)
     if search_form.is_valid():
         materiaux_criteria = search_form.cleaned_data["materiaux"]
+        criteria = re.findall(materiaux_criteria)
         materiaux = []
-        materiaux += Materiau.objects.filter(nom__contains=materiaux_criteria)
-        materiaux += Materiau.objects.filter(slug__contains=materiaux_criteria)
-        materiaux += Materiau.objects.filter(usage__contains=materiaux_criteria)
-        materiaux += Materiau.objects.filter(fournisseur__contains=materiaux_criteria)
+        for crit in criteria:
+            materiaux += Materiau.objects.filter(nom__contains=crit)
+            materiaux += Materiau.objects.filter(slug__contains=crit)
+            materiaux += Materiau.objects.filter(usage__contains=crit)
+            materiaux += Materiau.objects.filter(fournisseur__contains=crit)
         return render_to_response("materiautheque/results.html", {"materiaux": materiaux, "cri": materiaux_criteria})
     return render(request, 'materiautheque/homepage.html', {'last_matx': last_matx, "form": search_form})
